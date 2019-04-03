1c72eca702c6e3729c6c634573836b9375e8ccf2
Michel Brüger <michel.brueger@haw-hamburg.de>
2019-04-03 23:16:43 +0200

    fileFolder String changed

--- src/main/java/com/michel/brueger/ConversionTool.java
+++ src/main/java/com/michel/brueger/ConversionTool.java
@@ -8,13 +8,14 @@ import java.util.regex.Pattern;
 public class ConversionTool {
 
     public void readGraphFile(String filename) throws IOException {
-        String filesFolder = "/Users/michelbrueger/IdeaProjects/gkaPraktikum1/src/main/files/";
+//        String filesFolder = "/Users/michelbrueger/IdeaProjects/gkaPraktikum1/src/main/files/";
+        String filesFolder = "src/main/files/";
         FileReader fileReader = new FileReader(filesFolder+filename);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         Graph resultGraph = new Graph();
 
         String line = bufferedReader.readLine();
-
+// /Users/michelbrueger/IdeaProjects/gkaPraktikum1/src/main/java/com/michel/brueger/ConversionTool.java
         while (line != null) {
             System.out.println(line);                                                                      // DebugSout
             if (line.equals("#directed")) resultGraph.setDirected();
